579237a84dc12252440efcc830cb97313058a0af
강휘 <kh@paytalab.com>
2024-05-27 19:16:48 +0900

    project init

---
+++ UserList/NetworkManager.swift
@@ -0,0 +1,12 @@
+//
+//  Network.swift
+//  UserList
+//
+//  Created by paytalab on 5/27/24.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+}
---
+++ UserList/UserListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  UserListViewController.swift
+//  UserList
+//
+//  Created by paytalab on 5/27/24.
+//
+
+import UIKit
+
+class UserListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
